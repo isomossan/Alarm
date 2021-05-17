7aa9fb3e86ee6239f27bf634d0940f99ea6fece0
isomoto masaki <83933546+isomossan@users.noreply.github.com>
2021-05-17 15:37:03 +0900

    Create timer.js

---
+++ timer.js
@@ -0,0 +1,30 @@
+const H = document.getElementById('h');
+const M = document.getElementById('m');
+
+
+function timer(){
+var nowTime = new Date(); //  現在日時を得る
+var nowHour = nowTime.getHours(); // 時を抜き出す
+var nowMin  = nowTime.getMinutes(); // 分を抜き出す
+var nowSec = nowTime.getSeconds(); // 秒を抜き出す
+  
+var msg = nowHour + ":" + nowMin + "." + nowSec;　//表示する時間(時間+分)
+document.getElementById("time-display").innerHTML = msg; //時間を表示させる
+
+var time = nowHour + ":" + nowMin + "," + nowSec;　//裏で使う時間(時間+分+秒)
+
+
+    const Hours = H.value; //inputから時値を抜き出す
+    const Minures = M.value; //inputから分値を抜き出す
+
+if ( Hours + ':' + Minures + ',0' == time) {  
+    confirm('時間だよ')  
+    }
+
+
+    console.log(Hours + ':' + Minures);
+};
+
+setInterval(timer,500)
+
+
