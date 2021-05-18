5095512d84c321ee322b5a01befd191add0fe7f0
isomoto masaki <83933546+isomossan@users.noreply.github.com>
2021-05-18 09:42:39 +0900

    Update timer.js

--- timer.js
+++ timer.js
@@ -18,7 +18,7 @@ var time = nowHour + ":" + nowMin + "," + nowSec;　//裏で使う時間(時間+
     const Minures = M.value; //inputから分値を抜き出す
 
 if ( Hours + ':' + Minures + ',0' == time) {  
-    confirm('時間です')  
+    confirm('時間だよ')  
     }
 
 
